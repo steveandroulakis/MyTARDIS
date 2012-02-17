3cd34e3e721fc0d4ffdedc2c8f33c0e4e0da36b0
Steve Androulakis <steve.androulakis@gmail.com>
2012-02-17 11:51:10 +1100

    added celery task for sending email asynchronously

--- tardis/tardis_portal/tasks.py
+++ tardis/tardis_portal/tasks.py
@@ -1,7 +1,16 @@
 from celery.task import Task
 from celery.registry import tasks
 from tardis.tardis_portal.forms import ExperimentForm
+from django.conf import settings
 from django.core.mail import send_mail
+
+class SendMail(Task):
+    def run(self, subject, body, to, **kwargs):
+        
+        send_mail(subject, body, settings.EMAIL_HOST_USER,
+            to, fail_silently=False)
+
+tasks.register(SendMail)
 
 class SaveExperiment(Task):
     def run(self, post, files, user, instance=None, extra=0, **kwargs):
@@ -16,14 +25,25 @@ class SaveExperiment(Task):
                 
                 full_experiment.create_default_ACL(user)
                 
-                send_mail('Experiment Creation Successful', 'Yay \o/ ' + str(full_experiment['experiment'].id), 'steve.androulakis@gmail.com',
-                    ['steve.androulakis@gmail.com'], fail_silently=False)
+                subject = 'Experiment Creation Successful'
+                body = 'Yay \o/ ' + str(full_experiment['experiment'].id)
+                to = [user.email,]
+                
+                SendMail.delay(subject=subject,
+                    body=body,
+                    to=to)
+        
         else:
             ef = ExperimentForm(post, files, instance=instance, extra=extra)
             if ef.is_valid():
                 full_experiment = ef.save(commit=False)
                 
-                send_mail('Experiment Save Successful', 'Woo \o/ ' + str(full_experiment['experiment'].id), 'steve.androulakis@gmail.com',
-                    ['steve.androulakis@gmail.com'], fail_silently=False)
+                subject = 'Experiment Save Successful'
+                body = 'Woo \o/ ' + str(full_experiment['experiment'].id)
+                to = [user.email,]
+                
+                SendMail.delay(subject=subject,
+                    body=body,
+                    to=to)
 
 tasks.register(SaveExperiment)
