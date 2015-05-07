b7e33f55ba77ce0285023dfd6ce6f7618c60821c
Steve Androulakis <steve.androulakis@gmail.com>
2015-05-07 23:56:35 +1000

    Can copy_files() from tar box to default box now

--- tardis/tardis_portal/storage/tararchivefs.py
+++ tardis/tardis_portal/storage/tararchivefs.py
@@ -14,6 +14,7 @@ from django.utils.functional import LazyObject
 from django.utils.module_loading import import_by_path
 from django.utils.text import get_valid_filename
 from django.utils._os import safe_join, abspathu
+from django.core.files.storage import Storage
 import logging
 from tardis.tardis_portal.models import StorageBox, DataFileObject
 
@@ -55,135 +56,29 @@ def copy_file(self, dest_box=None, verify=True):
         copy.verify.delay()
     return copy
 
-class Storage(object):
-    """
-    A base storage class, providing some default behaviors that all other
-    storage systems can inherit or override, as necessary.
-    """
-
-    # The following methods represent a public interface to private methods.
-    # These shouldn't be overridden by subclasses unless absolutely necessary.
-
-    def open(self, name, mode='rb'):
-        """
-        Retrieves the specified file from storage.
-        """
-        return self._open(name, mode)
-
-    def save(self, name, content):
-        """
-        Saves new content to the file specified by name. The content should be
-        a proper File object or any python file-like object, ready to be read
-        from the beginning.
-        """
-        # Get the proper name for the file, as it will actually be saved.
-        if name is None:
-            name = content.name
-
-        if not hasattr(content, 'chunks'):
-            content = File(content)
-
-        name = self.get_available_name(name)
-        name = self._save(name, content)
-
-        # Store filenames with forward slashes, even on Windows
-        return force_text(name.replace('\\', '/'))
-
-    # These methods are part of the public API, with default implementations.
-
-    def get_valid_name(self, name):
-        """
-        Returns a filename, based on the provided filename, that's suitable for
-        use in the target storage system.
-        """
-        return get_valid_filename(name)
-
-    def get_available_name(self, name):
-        """
-        Returns a filename that's free on the target storage system, and
-        available for new content to be written to.
-        """
-        dir_name, file_name = os.path.split(name)
-        file_root, file_ext = os.path.splitext(file_name)
-        # If the filename already exists, add an underscore and a random 7
-        # character alphanumeric string (before the file extension, if one
-        # exists) to the filename until the generated filename doesn't exist.
-        while self.exists(name):
-            # file_ext includes the dot.
-            name = os.path.join(dir_name, "%s_%s%s" % (file_root, get_random_string(7), file_ext))
-
-        return name
-
-    def path(self, name):
-        """
-        Returns a local filesystem path where the file can be retrieved using
-        Python's built-in open() function. Storage systems that can't be
-        accessed using open() should *not* implement this method.
-        """
-        raise NotImplementedError("This backend doesn't support absolute paths.")
-
-    # The following methods form the public API for storage systems, but with
-    # no default implementations. Subclasses must implement *all* of these.
-
-    def delete(self, name):
-        """
-        Deletes the specified file from the storage system.
-        """
-        raise NotImplementedError()
-
-    def exists(self, name):
-        """
-        Returns True if a file referened by the given name already exists in the
-        storage system, or False if the name is available for a new file.
-        """
-        raise NotImplementedError()
-
-    def listdir(self, path):
-        """
-        Lists the contents of the specified path, returning a 2-tuple of lists;
-        the first item being directories, the second item being files.
-        """
-        raise NotImplementedError()
-
-    def size(self, name):
-        """
-        Returns the total size, in bytes, of the file specified by name.
-        """
-        raise NotImplementedError()
-
-    def url(self, name):
-        """
-        Returns an absolute URL where the file's contents can be accessed
-        directly by a Web browser.
-        """
-        raise NotImplementedError()
-
-    def accessed_time(self, name):
-        """
-        Returns the last accessed time (as datetime object) of the file
-        specified by name.
-        """
-        raise NotImplementedError()
-
-    def created_time(self, name):
-        """
-        Returns the creation time (as datetime object) of the file
-        specified by name.
-        """
-        raise NotImplementedError()
-
-    def modified_time(self, name):
-        """
-        Returns the last modified time (as datetime object) of the file
-        specified by name.
-        """
-        raise NotImplementedError()
-
 #TODOsteve utils
 def ensure_dir(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
+
+def newer_archive(tar_file, cache_file):
+    if not os.path.exists(cache_file):
+        print 'path %s does not exist' % cache_file
+        return True
+
+    return os.path.getmtime(tar_file) > os.path.getmtime(cache_file)
+
+
+def retrieve_tar_from_tape(tar_path, cache_path, dataset_id):
+    import shutil
+    tar_file = tar_path + '/' + dataset_id + '.tar'
+    cache_file = cache_path + '/' + dataset_id + '.tar'
+    # tmp tar in cache for extract
+    shutil.copy2(tar_file, cache_file)
+
+    return cache_file
+
 
 # todo add functions to
 class TarArchiveFileSystemStorage(Storage):
@@ -191,17 +86,20 @@ class TarArchiveFileSystemStorage(Storage):
     Standard filesystem storage
     """
 
-    def __init__(self, location=None, base_url=None, dataset_id=None,
+    def __init__(self, cache_path=None, base_url=None, dataset_id=None,
                  tar_path=None):
-        if location is None:
-            location = '/home/ubuntu/mytardis/var/tararchive_cache/'
-        self.base_location = location
-        self.location = abspathu(self.base_location)
+        # cache location
+        if cache_path is None:
+            cache_path = '/home/ubuntu/mytardis/var/tararchive_cache/'
+        self.base_location = cache_path
+        self.cache_path = abspathu(self.base_location)
         if base_url is None:
             base_url = settings.MEDIA_URL
         self.base_url = base_url
         self.dataset_id = dataset_id
         self.tar_path = tar_path
+        self.tar_file = self.tar_path + '/' + self.dataset_id + '.tar'
+        self.cache_file = self.cache_path + '/' + self.dataset_id + '.tar'
 
     '''
     In reality this won't happen from direct downloads as we don't want to
@@ -223,16 +121,22 @@ class TarArchiveFileSystemStorage(Storage):
     breaks the django-storage abstraction.
     '''
     def _open(self, name, mode='rb'):
-        import tarfile, os
+        import tarfile, shutil
 
-        tar = tarfile.open('/home/ubuntu/mytardis/var/tararchive/' + str(self.dataset_id) + '.tar')
+        if newer_archive(self.tar_file, self.cache_file):
+            print "archive newer or cache copy doesn't exist error"
+            raise IOError("%s tape archive is newer than disk copy, please retrieve"
+                          % self.dataset_id)
+
+        # open tar in cache for reading
+        tar = tarfile.open(self.cache_file)
 
         # filename = 'A dataset-1/Photo 4-10-12 2 15 00 PM.png'
         # filename = 'A dataset-1/SekZx.jpg'
 
         filename = name
 
-        write_path = self.location + filename
+        write_path = self.cache_path + '/' + filename
 
         member = tar.getmember(filename)
 
@@ -319,139 +223,6 @@ class TarArchiveFileSystemStorage(Storage):
         Deletes the specified file from the storage system.
         """
         raise NotImplementedError()
-
-    def exists(self, name):
-        return os.path.exists(self.path(name))
-
-    def listdir(self, path):
-        path = self.path(path)
-        directories, files = [], []
-        for entry in os.listdir(path):
-            if os.path.isdir(os.path.join(path, entry)):
-                directories.append(entry)
-            else:
-                files.append(entry)
-        return directories, files
-
-    def path(self, name):
-        try:
-            path = safe_join(self.location, name)
-        except ValueError:
-            raise SuspiciousFileOperation("Attempted access to '%s' denied." % name)
-        return os.path.normpath(path)
-
-    def size(self, name):
-        return os.path.getsize(self.path(name))
-
-    def accessed_time(self, name):
-        return datetime.fromtimestamp(os.path.getatime(self.path(name)))
-
-    def created_time(self, name):
-        return datetime.fromtimestamp(os.path.getctime(self.path(name)))
-
-    def modified_time(self, name):
-        return datetime.fromtimestamp(os.path.getmtime(self.path(name)))
-
-#TODOsteve add functions to
-#TODOsteve cache location hardcoded
-class TarArchiveFileSystemStorageCache(Storage):
-    """
-    Standard filesystem storage
-    """
-
-    def __init__(self, location=None, base_url=None):
-        if location is None:
-            location = '/home/ubuntu/mytardis/var/tararchive_cache'
-        self.base_location = location
-        self.location = abspathu(self.base_location)
-        if base_url is None:
-            base_url = settings.MEDIA_URL
-        self.base_url = base_url
-
-    def _open(self, name, mode='rb'):
-        return File(open(self.path(name), mode))
-
-    def _save(self, name, content):
-        full_path = self.path(name)
-
-        # Create any intermediate directories that do not exist.
-        # Note that there is a race between os.path.exists and os.makedirs:
-        # if os.makedirs fails with EEXIST, the directory was created
-        # concurrently, and we can continue normally. Refs #16082.
-        directory = os.path.dirname(full_path)
-        if not os.path.exists(directory):
-            try:
-                os.makedirs(directory)
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise
-        if not os.path.isdir(directory):
-            raise IOError("%s exists and is not a directory." % directory)
-
-        # There's a potential race condition between get_available_name and
-        # saving the file; it's possible that two threads might return the
-        # same name, at which point all sorts of fun happens. So we need to
-        # try to create the file, but if it already exists we have to go back
-        # to get_available_name() and try again.
-
-        while True:
-            try:
-                # This file has a file path that we can move.
-                if hasattr(content, 'temporary_file_path'):
-                    file_move_safe(content.temporary_file_path(), full_path)
-                    content.close()
-
-                # This is a normal uploadedfile that we can stream.
-                else:
-                    # This fun binary flag incantation makes os.open throw an
-                    # OSError if the file already exists before we open it.
-                    flags = (os.O_WRONLY | os.O_CREAT | os.O_EXCL |
-                             getattr(os, 'O_BINARY', 0))
-                    # The current umask value is masked out by os.open!
-                    fd = os.open(full_path, flags, 0o666)
-                    _file = None
-                    try:
-                        locks.lock(fd, locks.LOCK_EX)
-                        for chunk in content.chunks():
-                            if _file is None:
-                                mode = 'wb' if isinstance(chunk, bytes) else 'wt'
-                                _file = os.fdopen(fd, mode)
-                            _file.write(chunk)
-                    finally:
-                        locks.unlock(fd)
-                        if _file is not None:
-                            _file.close()
-                        else:
-                            os.close(fd)
-            except OSError as e:
-                if e.errno == errno.EEXIST:
-                    # Ooops, the file exists. We need a new file name.
-                    name = self.get_available_name(name)
-                    full_path = self.path(name)
-                else:
-                    raise
-            else:
-                # OK, the file save worked. Break out of the loop.
-                break
-
-        if settings.FILE_UPLOAD_PERMISSIONS is not None:
-            os.chmod(full_path, settings.FILE_UPLOAD_PERMISSIONS)
-
-        return name
-
-    def delete(self, name):
-        assert name, "The name argument is not allowed to be empty."
-        name = self.path(name)
-        # If the file exists, delete it from the filesystem.
-        # Note that there is a race between os.path.exists and os.remove:
-        # if os.remove fails with ENOENT, the file was removed
-        # concurrently, and we can continue normally.
-        if os.path.exists(name):
-            try:
-                os.remove(name)
-            except OSError as e:
-                if e.errno != errno.ENOENT:
-                    raise
 
     def exists(self, name):
         return os.path.exists(self.path(name))
